44b387478aaa7b2584c64c948705e2ec1c3d3837
doctor-rutvik14 <rutvik1999149@gmail.com>
2024-07-25 10:28:09 -0300

    Updates..

--- main1.js
+++ main1.js
@@ -1,10 +1,10 @@
 // guess the o/p
-// practice questions
-//🍌🍎🍇🍊
+// practice question 
 const fruit = ['🍎', '🍇', '🍊'];
 
-fruit.slice(0, 1);
-fruit.splice(0, 1);
-fruit.unshift("🍌");
+fruit.slice(0, 1); // This will return index 0 to index 1 (not inclusive) of the original array i.e; first element of an array '🍎'. However, this does not modify the original fruit array.
+fruit.splice(0, 1); // Removes 1 element at index 0, which is '🍎'
+fruit.unshift("🍌"); // unshift method will add "🍌" to the beginning of an array.
 
 console.log(fruit);
+// Answer ["🍌", "🍇", "🍊"];
