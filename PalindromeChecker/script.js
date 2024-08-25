88854a22f16faf06cc2b6b963c5c19fe7ad4f839
doctor-rutvik14 <rutvik1999149@gmail.com>
2024-08-25 15:41:31 -0300

    Added comments

--- PalindromeChecker/script.js
+++ PalindromeChecker/script.js
@@ -1,11 +1,11 @@
 const input = document.getElementById("input");
 
 /* This function helps us to reverse the string.
-split() function acts like this:
+- split() function acts like this:
 dad -> ["d", "a", d] converting string to array.
-reverse() will reverse the input array
+- reverse() will reverse the input array
 ["h", "e", "l", "l", "o"] -> ["o", "l", "l", "e", "h"]
-join() function will combine the individual characters to a string "olleh"
+- join() function will combine the individual characters to a string "olleh"
 */
 
 function reversString(st) {
@@ -13,8 +13,8 @@ function reversString(st) {
 }
 /*
 For the variable value it first retrives the input value from the field
-trim() removes any leading and trailing whitespace from the input string.
-.toLowerCase() converts the trimmed string to lowercase to ensure the palindrome check is case-insensitive.
+- .trim() removes any leading and trailing whitespace from the input string.
+- .toLowerCase() converts the trimmed string to lowercase to ensure the palindrome check is case-insensitive.
 */
 
 function check() {
