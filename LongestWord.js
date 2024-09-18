b9f226b0309bcbef1650307e41806cea7f011e4e
doctor-rutvik14 <rutvik1999149@gmail.com>
2024-09-18 20:06:10 -0300

    new script added

---
+++ LongestWord.js
@@ -0,0 +1,24 @@
+/*Question: Longest word in a string*/
+
+// Write a function to findalongest that takes string as an input and returns the longest word in the string. If there are multiple longest words, return the first encountered.
+
+// Constraints:
+//1. Input string can contain alphabetic characters, digits, spaces and punctuation.
+//2. The Input string is non-empty
+//3. The input string can contain multiple words seperatd by spaces.
+
+//Note:
+// If input string is empty or contains whitespace, the function should return as false.
+// The function should ignore the white spaces when finding the longest word.
+
+const findLongestWord = (str) => {
+  if (str.trim().length === 0) {
+    return false;
+  }
+  let strArr = str.split(" ");
+  strArr = strArr.sort((a, b) => a.length - b.length); // It gets sort on the basis of Unicode and it starts eith capital letters.
+  console.log(strArr);
+  return strArr.at(-1);
+};
+
+console.log(findLongestWord("Hello, How are you doing Rutvik ?"));
