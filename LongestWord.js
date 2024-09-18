b0459c5807fe978ff68b9a510d8964eaedb4cfcd
doctor-rutvik14 <rutvik1999149@gmail.com>
2024-09-18 20:17:56 -0300

    new script added

--- LongestWord.js
+++ LongestWord.js
@@ -16,9 +16,22 @@ const findLongestWord = (str) => {
     return false;
   }
   let strArr = str.split(" ");
-  strArr = strArr.sort((a, b) => a.length - b.length); // It gets sort on the basis of Unicode and it starts eith capital letters.
+  strArr = strArr.sort((a, b) => a.length - b.length); // It gets sort on the basis of Unicode and it starts with capital letters.
   console.log(strArr);
   return strArr.at(-1);
 };
 
 console.log(findLongestWord("Hello, How are you doing Rutvik ?"));
+// Solution using different way 
+
+// const findLongestWord = (str) => {
+//   if (str.trim().length === 0) {
+//     return false;
+//   }
+//   let arr = str.split(" ");
+//   arr = arr.sort((a, b) => b.length - a.length);
+//   console.log(arr);
+//   return arr.at(0);
+// }
+
+// console.log(findLongestWord("Hi, how are you doing?"));
